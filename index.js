e71da38702fb97465285fd9f8a75236a0e21a929
Minami Itsukaichi <mitukaichi@gmail.com>
2023-10-18 14:49:13 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What's the title of your project?",
+    default: "",
+    choices: [],
+  }
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
+
+
+/*
+
+var inquirer = require('inquirer');
+inquirer
+  .prompt([
+    // Your question
+])
+.then((answers) => {
+  // answer
+})
+.catch((error) => {
+  if (error.isTtyError) {
+    // Prompt couldn't be rendered in the current environment
+  } else {
+    // Something else went wrong
+  }
+});
+
+*/
